aa08b0d337f88069a592662a45c36bad75e0f70c
tranguyen95 <nguyenhuongtra24@gmail.com>
2023-09-13 20:28:07 +0700

    init source fe

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { Suspense, lazy, useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import ECommerce from './pages/Dashboard/ECommerce';
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import routes from './routes';
+import Loader from './common/Loader';
+
+const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        containerClassName="overflow-auto"
+      />
+
+      <Routes>
+        <Route path="/auth/signin" element={<SignIn />} />
+        <Route path="/auth/signup" element={<SignUp />} />
+        <Route element={<DefaultLayout />}>
+          <Route index element={<ECommerce />} />
+          {routes.map(({ path, component: Component }, index : number) => (
+            <Route
+              key={index}
+              path={path}
+              element={
+                <Suspense fallback={<Loader />}>
+                  <Component />
+                </Suspense>
+              }
+            />
+          ))}
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
