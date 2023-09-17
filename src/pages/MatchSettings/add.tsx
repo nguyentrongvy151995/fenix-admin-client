8cef21454193995520cbb8a8efcbb7953cf9db1a
tranguyen95 <nguyenhuongtra24@gmail.com>
2023-09-17 23:38:54 +0700

    add demo nested array

---
+++ src/pages/MatchSettings/add.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import FieldArray from './fieldArray';
+import ReactDOM from 'react-dom';
+
+import './styles.css';
+
+const defaultValues = {
+  test: [
+    {
+      name: 'useFieldArray1',
+      nestedArray: [{ field1: 'field1', field2: 'field2' }],
+    },
+    {
+      name: 'useFieldArray2',
+      nestedArray: [{ field1: 'field1', field2: 'field2' }],
+    },
+  ],
+};
+
+export default function AddMatchSetting() {
+  const {
+    control,
+    register,
+    handleSubmit,
+    getValues,
+    formState: {errors},
+    reset,
+    setValue,
+  } = useForm({
+    defaultValues,
+  });
+  const onSubmit = (data : any) => console.log('data', data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Array of Array Fields</h1>
+      <p>
+        The following example demonstrate the ability of building nested array
+        fields.
+      </p>
+
+      <FieldArray
+        {...{ control, register, defaultValues, getValues, setValue, errors }}
+      />
+
+      <button type="button" onClick={() => reset(defaultValues)}>
+        Reset
+      </button>
+
+      <input type="submit" />
+    </form>
+  );
+}
---
+++ src/pages/MatchSettings/index.tsx
@@ -0,0 +1,3 @@
+export default function MatchSetting() {
+    return <h1>Match setting</h1>
+}
