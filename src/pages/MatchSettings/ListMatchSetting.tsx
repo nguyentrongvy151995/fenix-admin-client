18ddac90978879897b31f194bb012d4ef51c4b54
tranguyen95 <nguyenhuongtra24@gmail.com>
2023-09-24 22:26:56 +0700

    update css pagination

--- src/pages/MatchSettings/ListMatchSetting.tsx
+++ src/pages/MatchSettings/ListMatchSetting.tsx
@@ -202,7 +202,7 @@ const TableMatchSettings = (props: any) => {
                         (props.currentPage == index + 1
                           ? 'bg-[#3b50e0] text-[#fff] border-[#3b50e0]'
                           : '') +
-                        ` flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`
+                        ` flex items-center justify-center px-3 h-8 leading-tight text-gray-50  border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`
                       }
                     >
                       {index + 1}
